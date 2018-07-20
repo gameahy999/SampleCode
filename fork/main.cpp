6afe969a02991306bb4ffa3879c73389726f2616
anh <anh@vmware.com>
2018-07-20 14:36:38 +0800

    waitpid & getpid
    Signed-off-by: anh <anh@vmware.com>

--- fork/main.cpp
+++ fork/main.cpp
@@ -5,6 +5,8 @@
 
 void ChildProcess()
 {
+   printf("c=> pid: %d\n", getpid());
+   printf("c=> ppid: %d\n", getppid());
    printf("ChildProcess begin\n");
    sleep(3);
    printf("ChildProcess end\n");
@@ -12,6 +14,7 @@ void ChildProcess()
 
 void ParentProcess()
 {
+   printf("p=> pid: %d\n", getpid());
    printf("ParentProcess\n");
    printf("before wait\n");
    waitpid(-1, NULL, 0);
@@ -20,13 +23,23 @@ void ParentProcess()
 
 int main()
 {
+   printf("main entry pid: %d\n", getpid());
+
    pid_t pid = fork();
 
    if (pid == 0) {
       ChildProcess();
    } else {
+      printf("This pid is for child: %d\n", (int) pid);
       ParentProcess();
    }
 
    return 0;
 }
+
+/*
+ * Summary:
+ *    1. fork will return twice:
+ *       one for child(pid == 0), one for parent(not 0, but child's pid)
+ *    2. any process can discover its own PID by calling getpid().
+ */
---
+++ waitpid/main.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+void ChildProcess(int tag, int s)
+{
+   printf("ChildProcess%d begin\n", tag);
+   sleep(s);
+   printf("ChildProcess%d end\n", tag);
+}
+
+void ParentProcess()
+{
+   printf("ParentProcess\n");
+   printf("before wait\n");
+   waitpid(-1, NULL, 0);
+   printf("after wait\n");
+}
+
+int main()
+{
+   pid_t pid1 = fork();
+
+   if (pid1 == 0) {
+      ChildProcess(1, 3);
+   } else {
+      // ParentProcess();
+      pid_t pid2 = fork();
+      if (pid2 == 0) {
+         ChildProcess(2, 5);
+      } else {
+         ParentProcess();
+      }
+   }
+
+   return 0;
+}
+
+/*
+ * Summary:
+ *    1. waitpid(-1, NULL, 0) wait for any sub-process, it will return if one of
+ *       them exit.
+ */
